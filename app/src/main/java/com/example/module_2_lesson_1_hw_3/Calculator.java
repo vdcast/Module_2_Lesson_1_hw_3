246e3228eb0a568af9e814f13a567b39c1e99364
VD <vdfree448@protonmail.com>
2022-06-30 17:16:14 +0300

    circle area basic logic

---
+++ app/src/main/java/com/example/module_2_lesson_1_hw_3/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.module_2_lesson_1_hw_3;
+
+public class Calculator {
+    public double circleArea(double radius){
+        double pi = 3.14;
+        return (pi * radius * radius);
+    }
+}
