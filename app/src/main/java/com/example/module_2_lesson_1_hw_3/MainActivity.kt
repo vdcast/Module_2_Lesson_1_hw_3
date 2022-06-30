3f177a9554c6971694a1f1b799be96d12ba79225
VD <vdfree448@protonmail.com>
2022-06-30 17:25:44 +0300

    dev merged to master

--- app/src/main/java/com/example/module_2_lesson_1_hw_3/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_1_hw_3/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        Log.d("MYLOG", "All merged successfully.")
 
     }
 }
