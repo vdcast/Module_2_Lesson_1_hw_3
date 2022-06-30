ca9abefca0518483ff954e4a043df7e959955abb
VD <vdfree448@protonmail.com>
2022-06-30 17:24:51 +0300

    circle, square and rectangle merged to dev

--- app/src/main/java/com/example/module_2_lesson_1_hw_3/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_1_hw_3/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.module_2_lesson_1_hw_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
